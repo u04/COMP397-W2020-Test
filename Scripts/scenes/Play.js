8591179477d6aff0ab794df0f126102ffb39f4b1
cdey1 <300920267@cencol.ca>
2020-02-22 13:41:06 -0500

    first commit

---
+++ Scripts/scenes/Play.js
@@ -0,0 +1,41 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var scenes;
+(function (scenes) {
+    var Play = /** @class */ (function (_super) {
+        __extends(Play, _super);
+        // PRIVATE INSTANCE MEMBERS
+        // PUBLIC PROPERTIES
+        // CONSTRUCTOR
+        function Play() {
+            var _this = _super.call(this) || this;
+            _this.Start();
+            return _this;
+        }
+        // PRIVATE METHODS
+        // PUBLIC METHODS
+        //initialize and instatiate
+        Play.prototype.Start = function () {
+            this.Main();
+        };
+        Play.prototype.Update = function () {
+        };
+        Play.prototype.Main = function () {
+        };
+        return Play;
+    }(objects.Scene));
+    scenes.Play = Play;
+})(scenes || (scenes = {}));
+//# sourceMappingURL=Play.js.map
---
+++ Scripts/scenes/Play.ts
@@ -0,0 +1,44 @@
+module scenes
+{
+    export class Play extends objects.Scene
+    {
+        // PRIVATE INSTANCE MEMBERS
+ 
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTOR
+        constructor()
+        {
+            super();
+
+            this.Start();
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+
+        //initialize and instatiate
+        public Start(): void 
+        {
+            
+
+            
+             this.Main();
+        }        
+        
+        public Update(): void 
+        {
+
+        }
+        
+        public Main(): void 
+        {
+
+
+        }
+
+        
+    }
+}
